0f324e8a60c6988fbfeaee161bccc151efcbdbcb
Yuslash <sibikrishna71@gmail.com>
2025-01-07 21:40:49 +0530

    removed the background and added the opacity

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,4 +3,10 @@ require("plugins.plugins")
 require("plugins.luasnip")
 require("plugins.cyber")
 vim.cmd("colorscheme cyberdream")
+-- vim.opt.guicursor = "n-v-c:block"
 
+-- Set cursor styles and behaviors for different modes
+vim.opt.guicursor = "n-v-c:block,i:block-CursorInsert,r-cr:hor20,o:hor50"
+
+-- Customize the highlight group for Insert mode cursor
+vim.api.nvim_set_hl(0, "CursorInsert", { fg = "white", bg = "blue" })
--- nvim/lua/plugins/cyber.lua
+++ nvim/lua/plugins/cyber.lua
@@ -3,5 +3,22 @@ return {
     "scottmckendry/cyberdream.nvim",
     lazy = false,
     priority = 1000,
+    config = function()
+      require("cyberdream").setup({
+        -- Check the theme's documentation for options
+        transparent = true, -- Use this if supported
+      })
+
+      -- Apply the theme
+      vim.cmd.colorscheme("cyberdream")
+
+      -- Remove background manually if the theme doesn't support transparency
+      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+      -- Optional: Adjust other highlight groups if needed
+      vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+      vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+    end,
   },
 }
